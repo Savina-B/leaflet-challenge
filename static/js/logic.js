753e0cde8bd1ac184481fb0ef18b54a1d3a05c57
Savina.B <saviboateng@gmail.com>
2023-09-21 10:30:28 +0100

    Amended html, js and css files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,68 +1,110 @@
-// Function to determine marker color based on depth
-function getColorForDepth(depth) {
-    // color ranges based on depth
-    var colorRanges = [
-        [0, 'lightgreen'],
-        [10, 'green'],
-        [30, 'yellow'],
-        [50, 'orange'],
-        [70, 'red'],
-        [90, 'darkred']
-    ];
+// Initialize the map
+var myMap = L.map("map", {
+    center: [0, 0],
+    zoom: 2,
+  });
+  
+  //Ttile layer
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(myMap);
+  
+  // Loading GeoJSON data
+  var geoJSONURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"; // Replace with the URL you obtained
+  fetch(geoJSONURL)
+    .then((response) => response.json())
+    .then((data) => {
+      // Setting marker size based on magnitude of earthquake
+      function getMarkerSize(magnitude) {
+        return magnitude * 5;
+      }
+  
+      // Setting marker color according to earthquake depth
+      function getMarkerColor(depth) {
+        // Customising colours
+        if (depth < 10) return "green";
+        else if (depth < 30) return "yellow";
+        else if (depth < 50) return "orange";
+        else return "red";
+      }
+  
+     // Legend
+  var legend = L.control({ position: "bottomright" });
 
-    // Loop through the color ranges and return the appropriate color
-    for (var i = 0; i < colorRanges.length; i++) {
-        if (depth <= colorRanges[i][0]) {
-            return colorRanges[i][1];
-        }
-    }
-    return 'black'; // Default color if depth is above the specified ranges
-}
+legend.onAdd = function () {
+  var div = L.DomUtil.create("div", "info legend");
+  var depthRanges = []; 
 
-// Initializing map
-var map = L.map('map').setView([0, 0], 2);
-
-// Adding tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// earthquake data from JSON
-d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson', function(error, data) {
-    if (error) throw error;
-
-    //  create markers
-    data.features.forEach(function(feature) {
-        var magnitude = feature.properties.mag;
-        var depth = feature.geometry.coordinates[2];
-        var markerSize = magnitude * 5; 
-        var markerColor = getColorForDepth(depth);
-
-        var marker = L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-            radius: markerSize,
-            fillColor: markerColor,
-            color: '#000',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        }).addTo(map);
-
-        marker.bindPopup(`Magnitude: ${magnitude}<br>Depth: ${depth.toFixed(2)} km<br>Location: ${feature.properties.place}`);
+  // Determining depth categories and colours for earthquake data
+  data.features.forEach(function (feature) {
+    var depth = feature.geometry.coordinates[2];
+    var color = getMarkerColor(depth);
+    
+    // Does depthrange already exist?
+    var existingRange = depthRanges.find(function (range) {
+      return range.color === color;
     });
 
-    // Legend
-    var legend = L.control({ position: 'bottomright' });
-    legend.onAdd = function(map) {
-        var div = L.DomUtil.create('div', 'info legend');
-        var depthRanges = [0, 10, 30, 50, 70, 90];
-    
-        for (var i = 0; i < depthRanges.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + getColorForDepth(depthRanges[i] + 1) + '"></i> ' +
-                depthRanges[i] + (depthRanges[i + 1] ? '&ndash;' + depthRanges[i + 1] + '<br>' : '+');
-        }
-        return div;
-    };
-    
-    legend.addTo(map);
-});
+    if (!existingRange) {
+      // Adding a new range if non existent
+      depthRanges.push({ color: color, minDepth: depth, maxDepth: depth });
+    } else {
+      // If yes, update the existing range's minDepth and maxDepth
+      if (depth < existingRange.minDepth) {
+        existingRange.minDepth = depth;
+      }
+      if (depth > existingRange.maxDepth) {
+        existingRange.maxDepth = depth;
+      }
+    }
+  });
+
+  // Generate legend HTML
+  depthRanges.forEach(function (range) {
+    div.innerHTML +=
+      '<i style="background:' +
+      range.color +
+      '"></i> ' +
+      range.minDepth +
+      ' - ' +
+      range.maxDepth +
+      ' km<br>';
+  });
+
+  return div;
+};
+
+legend.addTo(myMap);
+
+  
+      //  GeoJSON layer and custom styling
+      L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+          var magnitude = feature.properties.mag;
+          var depth = feature.geometry.coordinates[2];
+  
+          var markerOptions = {
+            radius: getMarkerSize(magnitude),
+            fillColor: getMarkerColor(depth),
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8,
+          };
+  
+          return L.circleMarker(latlng, markerOptions);
+        },
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup(
+            "<h3>Location: " +
+              feature.properties.place +
+              "</h3><hr><p>Magnitude: " +
+              feature.properties.mag +
+              "</p><p>Depth: " +
+              feature.geometry.coordinates[2] +
+              "</p>"
+          );
+        },
+      }).addTo(myMap);
+    });
+  
